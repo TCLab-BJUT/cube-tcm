5743df5ab3f059d91cc35d62967da57208b9d182
Hujun <algorist@bjut.edu.cn>
2019-01-23 15:20:43 +0800

    add Tpm_Getcapability 05 04 0124 emu

--- module/tpm_init_channel/tpm_init_channel.c
+++ module/tpm_init_channel/tpm_init_channel.c
@@ -450,6 +450,16 @@ int tpm_ordemu_GetCapability(struct vtcm_external_input_command * input_head,BYT
 				Memcpy(tpm_out->resp,out_data,tpm_out->respSize);
 			}	
 			break;					
+			case 0x0124:
+			{
+				BYTE out_data[16]={0x00,0x00,0x10,0x00}; 
+				tpm_out->paramSize=0x12;
+				tpm_out->returnCode=0;
+				tpm_out->respSize=0x04;
+				tpm_out->resp=Talloc0(tpm_out->respSize);
+				Memcpy(tpm_out->resp,out_data,tpm_out->respSize);
+			}	
+			break;					
 			default:
 				return -EINVAL;
 		}
@@ -457,7 +467,7 @@ int tpm_ordemu_GetCapability(struct vtcm_external_input_command * input_head,BYT
 	else if(tpm_in->capArea==0x1a)
 	{
 			BYTE out_data[16]={0x00,0x30,0x01,0x01,0x00,0x00,0x00,0x01,0x01,0x01,0x23,0x45,0x67,0x00,0x00}; 
-			tpm_out->paramSize=0x1A;
+			tpm_out->paramSize=0x1C;
 			tpm_out->returnCode=0;
 			tpm_out->respSize=0x0E;
 			tpm_out->resp=Talloc0(tpm_out->respSize);
