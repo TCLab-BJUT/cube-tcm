b8b11ce1653fc8d38659a11590d8874294511e03
Hu Jun <algorist@bjut.edu.cn>
2019-07-17 22:09:01 -0700

    a clean run_cube script

--- run_cube.sh
+++ run_cube.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+export CUBE_PATH=/root/centoscloud/cube-1.3
+export CUBE_TCM_PATH=`pwd`
+export LD_LIBRARY_PATH=$CUBE_PATH/cubelib/lib/:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=`pwd`/locallib/bin:$LD_LIBRARY_PATH
 $CUBE_PATH/proc/main/envset_proc $1 $2 $3 
